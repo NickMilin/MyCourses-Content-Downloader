2f91e5ff62cc25970c86bead912f59a94fe36374
NickMilin <nicholas.milin@gmail.com>
2025-01-25 12:39:43 -0500

    Create MyCoursesScraper.py
    Created start of project

---
+++ MyCoursesScraper.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+website = 'https://mycourses2.mcgill.ca/d2l/loginh/'
+path = 'YOUR CHROMEDRIVER PATH HERE'
+service = Service(path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
+driver.maximize_window()
+
+
