7135711db799d7f0e2af89ca16dbe3b3fd8ced70
NickMilin <nicholas.milin@gmail.com>
2025-01-25 16:59:09 -0500

    Update MyCoursesScraper.py
    Gets the url for each course

--- MyCoursesScraper.py
+++ MyCoursesScraper.py
@@ -68,10 +68,35 @@ while len(enrollment_cards) != len(new_cards):
     enrollment_cards = class_route_3.find_elements(By.XPATH, './/d2l-enrollment-card')
 
 time.sleep(1)
-
-
-
 print(class_route_3.text)
+
+# Now go into the web pages
+driver.execute_script("arguments[0].click();", enrollment_cards[0])
+
+# Now get go into one of the enrolled course
+enrollment_card_shadow = expand_shadow_element(enrollment_cards[0])
+# 3. Find the nested d2l-card inside the enrollment card's shadow DOM
+d2l_card = enrollment_card_shadow.find_element(By.CSS_SELECTOR, "d2l-card")
+
+# 4. Expand d2l-card's shadow root
+d2l_card_shadow = expand_shadow_element(d2l_card)
+
+# 5. Extract the link from the <a> tag inside the card's shadow DOM
+link_element = d2l_card_shadow.find_element(By.CSS_SELECTOR, "a[href]")
+course_url = link_element.get_attribute("href")
+
+print(course_url)
+
+'''
+# 6. Navigate to the extracted URL
+driver.get(course_url)
+
+
+navigation_list = driver.find_elements(By.XPATH, '//div[@class="d2l-navigation-s-item"]')
+
+'''
+
+
 
 
 
